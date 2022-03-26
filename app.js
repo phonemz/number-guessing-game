6661f9748db34b7a6a21b6d4c426af05b04b9a6e
Phonemz <phonemyintzaw3033@gmail.com>
2022-03-27 01:35:12 +0630

    whooooo ...

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const restart = document.querySelector("#restart")
 const guess = document.querySelector("#guessInput") 
 const guessOutput = document.querySelector("#previous")
 
-submit.addEventListener('click',()=> {
+submit.addEventListener('click', () => {
+    const newText = document.createElement("div")
+    newText.classList.add("previousGuess")
+    document.body.appendChild(newText)
+    newText.textContent = "Your previous guesses are"
+    console.log("Hello")
     guessOutput.innerHTML = guess.value 
 })
