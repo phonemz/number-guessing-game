fa02de34158f1d8ed334d3365300b62bbe159ce5
Phonemz <phonemyintzaw3033@gmail.com>
2022-03-29 01:28:28 +0630

    completed

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const hintText = document.querySelector('.hint')
 const alertMessage = document.querySelector('#alert-message')
 const previousGuessesAre = document.querySelector('.previous-guesses-are')
 
+guessInput.focus()
+
 let guessCount = 1
 
 function getRandom(a,b) {
@@ -15,37 +17,70 @@ function getRandom(a,b) {
 }
 
 const answer = getRandom(0, 100)
-console.log(answer)
 
-submit.addEventListener('click', () => {
+function stopGame() {
+    guessInput.disabled = 'true'
+    submit.disabled = 'true'
+}
 
+function restartGame() {
+    guessCount = 1
+    previousGuessesAre.textContent = ''
+    alertMessage.textContent = ''
+    guessInput.disabled = false
+    submit.disabled = false
+    guessInput.focus();
+}
+
+restart.addEventListener('click', restartGame)
+
+
+function mainFunction() {
     const value = parseInt(guessInput.value)
 
     if (guessCount === 1) {
         previousGuessesAre.textContent = 'Your previous guesses are '
     }
-    previousGuessesAre.textContent += guessInput.value + ' '     
+    previousGuessesAre.textContent += guessInput.value + ' '   
+    
+    if (guessCount > 10) {
+        alertMessage.textContent = 'Game Over. Please try again.'
+        stopGame()
+    }
 
     if (value === answer) {
         alertMessage.textContent = "Congratulations! Your guess is correct."
+        stopGame()
     } 
     else if (value > answer) {
-        alertMessage.textContent = "Your guess is higher than the answer"
+        alertMessage.textContent = "Your guess is higher than the answer."
     }
     else if (value < answer) {
-        alertMessage.textContent = "Your guess is lower than the answer"
+        alertMessage.textContent = "Your guess is lower than the answer."
     }
     
     
-
     if (value > 100) {
-        alertMessage.textContent = 'Please enter numbers between 0 and 100'
+        alertMessage.textContent = 'Please enter numbers between 0 and 100.'
     }
 
     if (value === '') {
-        alertMessage.textContent = 'Please ener a number'
+        alertMessage.textContent = 'Please ener a number.'
     }
 
     guessCount++
+    guessInput.value = ''
+    
+}
+
+submit.addEventListener('click', mainFunction)
+
+guessInput.addEventListener('keydown', function(e){
+    if (e.keyCode == 13) {
+        mainFunction()
+    }
+
+
 })
+
 
